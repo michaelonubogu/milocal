3c40ce3715c151cb82d202b2fc3fbde8e2e2a252
Michael Onubogu <nonubogu@gmail.com>
2015-10-06 11:14:55 -0400

    Update anuncios.js

--- js/anuncios.js
+++ js/anuncios.js
@@ -8,7 +8,28 @@ panelcontrol.factory("Auth", function($firebaseAuth) {
 anuncios.controller("AnunciosCtrl", function($scope, $firebaseArray){
     
         refanuncios = new Firebase("https://milocal.firebaseio.com/users/");
-        $scope.anuncio = $firebaseArray(refanuncios);   
+        //$scope.anuncio = $firebaseArray(refanuncios); 
+        $scope.anuncio = [];
+        
+        //(1) Here, I pull the data directly - without using $firebaseArray - u don't need $firebaseArray to use Angular with Firebase
+        $scope.anuncio = refanuncios.on('child_added', function(snapshot){
+          var post = snapshot.val();
+          $scope.anuncio.push(post);
+          //Get the userid and then use that to query comments
+          var userid = snapshot.key();
+          
+          //Now make a call to get comments
+          //... Have your code to get comments here... I don't think you should have comments in a separate controller
+          //..You pass the id of the user into the firebase orderBy method
+          var commentsRef = new Firebase("https://milocal.firebaseio.com/comments");
+          commentsRef
+            .orderByChild('userid')
+            .equalTo(userid)
+            .on('child_added', function(commentSnapshot){
+              //...process the comments
+              //var comment = commentSnapshot.val();
+            })
+        });
     
         $scope.showcomments = function(){
         console.log("Mostrando comentarios");
